f92a381a3431a320dfd87df24d705816882c52b7
Josh <aftershock840@gmail.com>
2024-12-28 17:46:45 -0500

    functions and render completed

--- js/assignment.js
+++ js/assignment.js
@@ -7,16 +7,27 @@ const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
 
-function returnTheRemainder(num1,num2) {
+function returnTheRemainder(num1, num2) {
     remainderElement.innerText = num1 % num2;
+};
+
+function checkIfEven( num = 47 ){
+    if(num % 2 === 0) {
+        checkIfEvenElement.innerText = "true";
+    } else {
+        checkIfEvenElement.innerText = "false";
+    };
+};
+
+function getTheFourthElement(arr) {
+      lostNumbersElement.innerText = arr[3];
 };
 
 function render(){
     returnTheRemainder(5,4);
-
+    checkIfEven();
+    getTheFourthElement(lostNumbers);
 };
-
-
 
 submissionBtn.addEventListener("click", function() {
     //Why does this return NaN?
