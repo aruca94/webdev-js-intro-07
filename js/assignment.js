8341287573fb6e38e74efa57178d9a0a4540f858
Josh <aftershock840@gmail.com>
2024-12-28 17:11:55 -0500

    remainder function, eventlistener and render function added

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,21 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemainder(num1,num2) {
+    let roundedNum = Math.round(num1/num2 % 2);
+    remainderElement.innerText = roundedNum;
+};
+
+function render(){
+    returnTheRemainder(5,4);
+
+};
+
+
+
+submissionBtn.addEventListener("click", function() {
+    //Why does this return NaN?
+    // returnTheRemainder();
+    render();
+});
