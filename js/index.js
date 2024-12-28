27ee9e40129ee4caf03637ab4171a19124e04c7a
Josh <aftershock840@gmail.com>
2024-12-28 13:17:39 -0500

    added experimental functions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+ "use strict"
 
+// function myFunc(num1,num2){
+//    return num1 + num2;
+// }
+// console.log(myFunc(1,5));
+
+// function otherFunc(phrase1 = "hello, ", phrase2 = "user") {
+//     // return phrase1 + phrase2;
+//     phrase1 + phrase2;
+// }
+// console.log(otherFunc());
