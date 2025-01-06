33c079660842941e4c1c990947fdf29152db12a6
Josh <aftershock840@gmail.com>
2025-01-06 09:42:58 -0500

    updated functions

--- js/assignment.js
+++ js/assignment.js
@@ -4,7 +4,6 @@ const submissionBtn = document.getElementById("submission-btn");
 const remainderElement = document.getElementById("remainder");
 const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
-
 const lostNumbers = [4, 8, 15, 16, 23, 42];
 
 function returnTheRemainder(num1, num2) {
@@ -30,7 +29,5 @@ function render(){
 };
 
 submissionBtn.addEventListener("click", function() {
-    //Why does this return NaN?
-    // returnTheRemainder();
     render();
 });
